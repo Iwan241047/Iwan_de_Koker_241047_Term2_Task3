54726d2fe054b5f6386f21aa5756ae114945a45c
Iwan241047 <241047@virtualwindow.co.za>
2024-05-22 15:30:29 +0200

    Added Javascript

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,38 @@
-document.addEventListener('DOMContentLoaded'){
-    let planet = {
-        
-    }
+//Initialising prices for the planets into an object
+const prices = {
+    mars: 25000,
+    neptune: 45000,
+    jupiter: 65000,
+    saturn: 40000,
+    uranus: 50000,
+    moon: 15000
+};
+
+//Initialising amount of tickets
+let ticketCounts = {
+    mars: 0,
+    neptune: 0,
+    jupiter: 0,
+    saturn: 0,
+    uranus: 0,
+    moon: 0
+};
+
+//Initialising the total price and tickets 
+let total = 0;
+let totalTickets = 0;
+
+//Calling ticket object's keys to iterate in loop
+let planets = Object.keys(ticketCounts);
+
+//Loop to add ticket count and prices
+for(let i = 0; i < planets.length; i++){
+    let planets = planets[i];
+    total += ticketCounts[planet] * prices[planets];
+    totalTickets += ticketCounts[planets];
 }
+
+console.log("Total Cost: R" + total);
+console.log("Total Flights Booked: " + totalTickets);
+
+
